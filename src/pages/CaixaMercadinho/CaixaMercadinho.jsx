53fd50931d68bfcb5005ef2a5f274f506985a807
RoneyAlmeida <70605483+RoneyAlmeida5@users.noreply.github.com>
2025-03-03 19:02:54 -0300

    firts commit

--- src/pages/CaixaMercadinho/CaixaMercadinho.jsx
+++ src/pages/CaixaMercadinho/CaixaMercadinho.jsx
@@ -1,6 +1,10 @@
 import api from "../../api/api";
 import "./CaixaMercadinho.css";
 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+
 import { useState } from "react";
 import logo from "../../assets/logo.png";
 
@@ -10,6 +14,16 @@ export default function CaixaMercadinho() {
   const [produto, setProduto] = useState("");
   const [valor, setValor] = useState("");
   const [formaPagamento, setFormaPagamento] = useState("Dinheiro");
+  const [adcProdOpen, setAdcProdOpen] = useState(false);
+  const [listProdOpen, setListProdOpen] = useState(false);
+
+  // ESTADOS DOS BOTÕES DO MODAL
+
+  const handleOpenAdcProd = () => setAdcProdOpen(true);
+  const handleCloseAdcProd = () => setAdcProdOpen(false);
+
+  const handleOpenListProd = () => setListProdOpen(true);
+  const handleCloseListProd = () => setListProdOpen(false);
 
   const adicionarProduto = () => {
     if (codigo && produto && valor) {
@@ -26,12 +40,98 @@ export default function CaixaMercadinho() {
     <div className="ContainerMerc">
       <img src={logo} alt="Logo da marca" className="ImgMerc" />
       <div className="ScrollContainer">
-        <button className="BtnScroll" onClick={() => []}>
+        <Button
+          sx={{
+            padding: "12px",
+            backgroundColor: "#0056b3",
+            color: "white",
+            border: "none",
+            borderRadius: "10px",
+            cursor: "pointer",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            "&:hover": {
+              backgroundColor: "#004199",
+            },
+          }}
+          onClick={handleOpenAdcProd}
+        >
           Adicionar produtos
-        </button>
-        <button className="BtnScroll" onClick={() => []}>
+        </Button>
+        <Button
+          sx={{
+            padding: "12px",
+            backgroundColor: "#0056b3",
+            color: "white",
+            border: "none",
+            borderRadius: "10px",
+            cursor: "pointer",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+            "&:hover": {
+              backgroundColor: "#004199",
+            },
+          }}
+          onClick={handleOpenListProd}
+        >
           Lista de produtos
-        </button>
+        </Button>
+        <Modal open={adcProdOpen} onClose={handleCloseAdcProd}>
+          <Box className="Box">
+            <div className="ScreenModalAdcProd">
+              <div className="PainelModalAdcProd">
+                <h2>ADICIONAR PRODUTOS</h2>
+                <input
+                  className="InputModalAdcProd"
+                  placeholder="Código"
+                  value={codigo}
+                  onChange={(e) => setCodigo(e.target.value)}
+                />
+                <input
+                  className="InputModalAdcProd"
+                  placeholder="Produto"
+                  value={produto}
+                  onChange={(e) => setProduto(e.target.value)}
+                />
+                <input
+                  className="InputModalAdcProd"
+                  placeholder="Valor"
+                  type="number"
+                  value={valor}
+                  onChange={(e) => setValor(e.target.value)}
+                />
+                <button className="Btn" onClick={adicionarProduto}>
+                  Adicionar
+                </button>
+              </div>
+            </div>
+          </Box>
+        </Modal>
+        <Modal open={listProdOpen} onClose={handleCloseListProd}>
+          <Box className="Box">
+            <div className="ScreenModalListProd">
+              <div className="PainelModalListProd">
+                <h2>PRODUTOS</h2>
+                <div className="TableModalListProd">
+                  <thead>
+                    <tr className="Tr">
+                      <th className="Th">Código</th>
+                      <th className="Th">Produto</th>
+                      <th className="Th">Valor</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {produtos.map((item, index) => (
+                      <tr className="Tr" key={index}>
+                        <td className="TdC">{item.codigo}</td>
+                        <td className="Td">{item.produto}</td>
+                        <td className="Td">R$ {item.valor.toFixed(2)}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </div>
+              </div>
+            </div>
+          </Box>
+        </Modal>
       </div>
       <div className="Screen">
         <div className="LeftPanel">
